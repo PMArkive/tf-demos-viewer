dd26cc9bce1699a0285f52755ed05fab82587dd1
Robin Appelman <robin@icewind.nl>
2019-12-22 17:36:27 +0100

    use new ticker

--- js/index.js
+++ js/index.js
@@ -11,7 +11,9 @@ import("../pkg/index.js")
                 console.log(reader.result);
                 let bytes = new Uint8Array(reader.result);
 
+                console.time('demo_parse');
                 m.parse_demo(bytes);
+                console.timeEnd('demo_parse');
             };
 
             reader.onerror = function() {
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,26 @@
+use tf_demo_parser::demo::parser::gamestateanalyser::GameStateAnalyser;
+use tf_demo_parser::{Demo, DemoParser, ParseError};
+use wasm_bindgen::__rt::std::time::Instant;
 use wasm_bindgen::prelude::*;
 use web_sys::console;
-use tf_demo_parser::{DemoParser, Demo};
 
 #[wasm_bindgen]
-pub fn parse_demo(buffer: &[u8]) {
+pub fn parse_demo(buffer: &[u8]) -> Result<(), JsValue> {
     let buffer = buffer.to_vec();
+    parse_demo_inner(buffer).map_err(|e| e.to_string().into())
+}
+
+pub fn parse_demo_inner(buffer: Vec<u8>) -> Result<(), ParseError> {
     console::log_1(&JsValue::from_str(&format!("len: {}", buffer.len())));
     let demo = Demo::new(buffer);
-    let (header, _) = DemoParser::parse_all(demo.get_stream()).unwrap();
+    let parser = DemoParser::new_with_analyser(demo.get_stream(), GameStateAnalyser::default());
+    let (header, mut ticker) = parser.ticker()?;
+    while ticker.tick()? {
+        // noop
+    }
+
     console::log_1(&JsValue::from_str(&format!("{:?}", header)));
+    Ok(())
 }
 
 // This is like the `main` function, except for JavaScript.
@@ -18,7 +30,6 @@ pub fn main_js() -> Result<(), JsValue> {
     // It's disabled in release mode so it doesn't bloat up the file size.
     #[cfg(debug_assertions)]
     console_error_panic_hook::set_once();
-
 
     // Your code goes here!
     console::log_1(&JsValue::from_str("Hello world!"));
